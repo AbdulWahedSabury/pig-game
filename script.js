b4533c4df9a5b49b2a7cec49b2da40782890c84b
Ab Sabury <sabury2020@gmail.com>
2022-10-18 11:18:38 -0700

    added finish game functionality

--- script.js
+++ script.js
@@ -21,22 +21,51 @@ score1El.textContent = 0;
 
 let courentNumber = 0;
 let activePlayer = 0;
+const scores = [0, 0];
+let gameStatus = true;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  courentNumber = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // dice role functionality
 btnRoll.addEventListener('click', function () {
-  // generate random roll
-  let score = Math.trunc(Math.random() * 6) + 1;
-  dice.classList.remove('hidden');
-  // display roll
-  dice.src = `dice-${score}.png`;
-  courentNumber += score;
-  // check if score is 1 or no
-  if (score !== 1) {
-    document.getElementById(`current--${activePlayer}`).textContent = courentNumber;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    courentNumber = 0;
-    activePlayer = (activePlayer === 0) ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+  if (gameStatus) {
+    // generate random roll
+    let score = Math.trunc(Math.random() * 6) + 1;
+    dice.classList.remove('hidden');
+    // display roll
+    dice.src = `dice-${score}.png`;
+    courentNumber += score;
+    // check if score is 1 or no
+    if (score !== 1) {
+      document.getElementById(`current--${activePlayer}`).textContent =
+        courentNumber;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (gameStatus) {
+    // add the courent score to total score
+    scores[activePlayer] += courentNumber;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // if total score >= 100 finish game
+    if (scores[activePlayer] >= 100) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      dice.classList.add('hidden');
+      gameStatus = false;
+    } else {
+      // switch player
+      switchPlayer();
+    }
   }
 });
